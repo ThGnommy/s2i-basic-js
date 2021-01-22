db6b33b853c5be6a526a6cb8cc9cb88e78772b7f
ThGnommy <th.brandoli@gmail.com>
2021-01-22 01:39:27 +0100

    css + fetch + funtions

--- js/main.js
+++ js/main.js
@@ -1,31 +1,73 @@
 const counterValue = document.getElementById("counter-value");
 const plus = document.getElementById("plus");
 const less = document.getElementById("less");
-const img = document.getElementById("img");
+const img = document.getElementById("bg");
+const progressBar = document.querySelector("progress");
 
 let value = 0;
+let imgSrc;
 let imgBlur = 100;
-
+let interval;
 counterValue.innerHTML = value + "%";
 img.style.filter = `blur(100px)`;
 
 const plusOne = () => {
-  if (value < 100) {
-    value++;
-    counterValue.innerHTML = value + "%";
-    img.style.filter = `blur(${imgBlur - value}px)`;
-    console.log(imgBlur - value);
-  }
+  interval = setInterval(() => {
+    if (value < 100) {
+      value++;
+      counterValue.innerHTML = value + "%";
+      img.style.filter = `blur(${imgBlur - value}px)`;
+      progressBar.value = value;
+      console.log(imgBlur - value);
+    }
+  }, 25);
 };
 
 const lessOne = () => {
-  if (value > 0) {
-    value--;
-    counterValue.innerHTML = value + "%";
-    img.style.filter = `blur(${imgBlur - value}px)`;
-    console.log(imgBlur - value);
-  }
+  interval = setInterval(() => {
+    if (value > 0) {
+      value--;
+      counterValue.innerHTML = value + "%";
+      img.style.filter = `blur(${imgBlur - value}px)`;
+      progressBar.value = value;
+    }
+  }, 25);
 };
 
-plus.addEventListener("click", () => plusOne());
-less.addEventListener("click", () => lessOne());
+plus.addEventListener("mousedown", () => plusOne());
+plus.addEventListener("mouseup", () => clearInterval(interval));
+plus.addEventListener("mouseleave", () => clearInterval(interval));
+
+plus.addEventListener("touchstart", () => plusOne());
+plus.addEventListener("touchend", () => clearInterval(interval));
+
+less.addEventListener("mousedown", () => lessOne());
+less.addEventListener("mouseup", () => clearInterval(interval));
+less.addEventListener("mouseleave", () => clearInterval(interval));
+
+less.addEventListener("touchstart", () => lessOne());
+less.addEventListener("touchend", () => clearInterval(interval));
+
+const getRandomImage = async () => {
+  await fetch(
+    "https://api.unsplash.com/photos/random/?" +
+      new URLSearchParams({
+        featured: true,
+        collections: "2292047, 3822610",
+      }),
+    {
+      headers: {
+        Authorization: "Client-ID Im2uZnSqdOdUJ6xaEuIfN-Ha5IDnUyobnntywEcqeR4",
+      },
+    }
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      imgSrc = data.urls.regular;
+      img.style.background = `url("${imgSrc}")`;
+    });
+};
+
+getRandomImage();
